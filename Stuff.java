5ac8df291fd3ff433571bfaff5790a0a5fcc0a9a
Attila Juhasz <106558542+attilamjuhasz@users.noreply.github.com>
2026-09-08 17:23:15 +0000

    found it

--- Stuff.java
+++ Stuff.java
@@ -8,23 +8,37 @@ import java.util.regex.Pattern;
 public class Stuff {
      public static void main(String[] args) throws IOException {
           String num = "12345";
+          int intNum = 12345;
           int i = 0;
           URL url = new URL("https://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=12345");
           while (i < 1000){
+               if (num.equals("peak.html")){
+                    break;
+               }
                url = new URL("https://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=" + num);
                BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
                
                String line = reader.readLine();
-               while (line != null){
-                    Pattern pat = Pattern.compile("[0-9]+");
-                    Matcher mat = pat.matcher(line);
-                    while(mat.find()) {
-                         System.out.println(line);
-                         System.out.println(mat.group());
-                         num = mat.group();
-                    }
-                    line = reader.readLine();
+               System.out.println(line);
+               System.out.println(intNum);
+               if (line.equals("Yes. Divide by two and keep going.")){
+                    intNum = intNum / 2;
+                    num = "" + intNum;
                     i++;
+               }
+               else {
+                    while (line != null){
+                         Pattern pat = Pattern.compile("[0-9]+");
+                         Matcher mat = pat.matcher(line);
+                         while(mat.find()) {
+                              System.out.println(line);
+                              System.out.println(mat.group());
+                              num = mat.group();
+                              intNum = Integer.parseInt(num);
+                         }
+                         line = reader.readLine();
+                         i++;
+                    }
                }
           }
      }
